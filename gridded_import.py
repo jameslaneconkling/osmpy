979f404239cf4dd521c99b6b8434eb096726343a
thomas-wri <tmaschler@wri.org>
2014-05-10 19:48:05 -0400

    makeSquare function

--- gridded_import.py
+++ gridded_import.py
@@ -1,7 +1,53 @@
 ### asdf
 
-def makeSquare():
-    ST_Extend(features)
+import psycopg2
+
+
+
+def makeSquare(conn, table, geom):
+
+    c = conn.cursor()
+    c.execute("SELECT ST_Extend(?) FROM ?", (geom, table)) 
+    row = c.fetchone()
+
+    bbox = row[1]
+
+    #PostGIS spits out something like this
+    #"BOX(968651.781199999 -434363.015500002,1667026.9567 257522.6459)"
+
+    minx = bbox[4:-1].split(',')[0].split(' ')[0]
+    miny = bbox[4:-1].split(',')[0].split(' ')[1]
+    maxx = bbox[4:-1].split(',')[1].split(' ')[0]
+    maxy = bbox[4:-1].split(',')[1].split(' ')[1]
+
+    width = maxx-minx
+    height = maxy-miny
+
+    dx = 0
+    dy = 0
+
+    #fat
+    if width > height:
+        dy = (width - height)/2
+    
+    #tall
+    elif width < height:
+        dx = (height - width)/2
+    #square
+    else:
+        pass
+
+    minx = minx - dx
+    miny = miny - dy
+    maxx = minx + dx
+    maxy = maxy + dy
+
+    
+    bbox = [minx,miny,maxx,maxy]
+
+    return bbox
+
+    
 
 def featureCount():
     pass
@@ -12,7 +58,14 @@ def splitBbox():
 def addToDB():
     pass
 
-bbox = makeSquare(features)
+#connetion to PostgreSQL
+conn = psycopg2.connect(database="testdb", user="postgres", password="pass123", host="127.0.0.1", port="5432")
+#Road table
+table = 'roads'
+#Geometry column of road table
+geom = 'geom'
+
+bbox = makeSquare(conn, table, geom)
 
 gridCells = [['1', bbox]]
 
